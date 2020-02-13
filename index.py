1b7a824c68dec9082d45d6cf2c2b51c93e4f5afb
SlipperyDan <djnostrom@gmail.com>
2020-02-13 14:14:18 -0700

    Add files via upload
    Changed from index.html to index.py. Moved from s3 cloud hosting static content to virtualized httpd hosting dynamic content. Technical depth, HERE I COME

---
+++ index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+print "Content-type: text/html\n\n"
+print "<html>\n<body>"
+print "<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">"
+print "Oh Snap, my concept is about to be proved"
+print "</div>\n</body>\n</html>"
